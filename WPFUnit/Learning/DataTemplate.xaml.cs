02e436068fad903a8c8697ae4de4b7e57161f37c
Tien Duy NGUYEN <39387827+TienDuyNGUYEN@users.noreply.github.com>
2019-11-04 23:56:06 +0100

    Data template

---
+++ WPFUnit/Learning/DataTemplate.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFUnit.Learning
+{
+    /// <summary>
+    /// Interaction logic for DataTemplate.xaml
+    /// </summary>
+    public partial class DataTemplate : Window
+    {
+        public DataTemplate()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn4_OnClick(object sender, RoutedEventArgs e)
+        {
+            string str1 = "HowKteam.com";
+            string str2 = "FreeEducation";
+            btn1.DataContext = str1;
+            btn2.DataContext = str2;
+        }
+    }
+}
---
+++ WPFUnit/Learning/Image.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFUnit.Learning
+{
+    /// <summary>
+    /// Interaction logic for Image.xaml
+    /// </summary>
+    public partial class Image : Window
+    {
+        public Image()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+           img.Source = new BitmapImage(new Uri(txbSource.Text));
+        }
+    }
+}
