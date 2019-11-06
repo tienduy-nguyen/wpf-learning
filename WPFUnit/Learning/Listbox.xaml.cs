3179fca421325263b9cc4a2e9943175ff5c0da9b
Tien Duy NGUYEN <tienduy.nguyen@users.noreply.github.com>
2019-11-06 09:31:10 +0100

    Listbox

---
+++ WPFUnit/Learning/Listbox.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFUnit.Learning
+{
+    /// <summary>
+    /// Interaction logic for Listbox.xaml
+    /// </summary>
+    public partial class Listbox : Window
+    {
+        private List<string> ListData;
+
+        public Listbox()
+        {
+            InitializeComponent();
+            ListData = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                ListData.Add(i.ToString());
+                cboData.ItemsSource = ListData;
+                lsbData.ItemsSource = ListData;
+            }
+        }
+    }
+}
